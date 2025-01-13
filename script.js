e2d88fd07fbb29b51f1d46dc4075d2883c776ce7
aya12sh <you@example.com>
2025-01-13 08:54:37 +0200

    update: add all files needed

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get DOM elements
+// Define possible images and outcomes
+// Event handler for image click
+// Reset results and display "Wait..."
+// Activate clicked image and deactivate others
+// Set user and bot images
+// Determine the result
+// Display the result
+// Attach event listeners to option images
